f8e30cc9df06690ddf9ebc421f06db28c1901e52
peekinternational <peekinternational@gmail.com>
2018-09-24 11:27:50 +0500

    start

---
+++ resources/views/frontend/includes/header.blade.php
@@ -0,0 +1,54 @@
+<header>
+  <div class="top_header hidden-xs">
+    <div class="container">
+      <div class="row">
+        <ul class="top_nav_2 pull-left hidden-sm">
+          <li><a href="#"><i class="fa fa-facebook"></i> </a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i> </a></li>
+          <li><a href="#"><i class="fa fa-linkedin"></i> </a></li>
+          <li><a href="#"><i class="fa fa-google"></i> </a></li>
+        </ul>
+        <ul class="top_nav pull-right">
+          <li><a href="#">00.00 USD $</a></li>
+          <li><a href="#">Message <span class="badge">5</span></a></li>
+          <li><a href="#">Notifications <span class="badge">17</span></a></li>
+          <li><a href="#"><i class="fa fa-wifi" aria-hidden="true"></i></a></li>
+          <li><a href="#"><i class="fa fa-th" aria-hidden="true"></i></a></li>
+          <li><a href="#"><img src="img/users/avatar-7.png"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-inverse"> <!-- main-navbar -->
+    <div class="container">
+      <div class="navbar-header"><!-- Brand and toggle -->
+        <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>-->
+        <a class="navbar-brand" href="index.html"><img src="img/logo.png" id="logo"></a>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Client <span class="caret"></span></a> </li>
+          <li><a href="#">Helper <span class="caret"></span></a> </li>
+          <li><a href="#">My Project <span class="caret"></span></a> </li>
+          <li><a href="#">get support <span class="caret"></span></a> </li>
+          <li>
+            <form>
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Get free quotes...">
+                <span class="input-group-btn">
+                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span> </button>
+            </span>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav><!-- main-navbar -->
+</header>
---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>Booking Yo</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		
+         <!-- Bootstrap checking -->
+        <link href="{{ asset('frontend-assets/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('frontend-assets/css/style.css') }}" rel="stylesheet">
+	    <link href="{{ asset('frontend-assets/css/font-awesome.css') }}" rel="stylesheet">
+        
+        <link href="{{ asset('frontend-assets/css/toastr.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+        @include('frontend.includes.header')
+
+        @yield('inner-header')
+
+        @yield('content')
+
+        @include('frontend.includes.footer')
+
+        <a href="#" class="job-notification">
+            <i class="fa fa-bell"></i>
+            <span class="notification-label">Subscribe for job notifications</span>
+        </a>
+
+        <a href="#" class="back-to-top" style="display: inline;">
+            <i class="fa fa-arrow-up"></i>
+        </a>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="{{ asset('frontend-assets/js/jquery.min.js') }}"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="{{ asset('frontend-assets/js/bootstrap.min.js') }}"></script>
+		
+
+        @yield('page-footer')
+    </body>
+</html>
